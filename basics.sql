4647714328a8daca43a61404d275eb7ed20ea420
HarshPanchigar <harshpanchigar@gmail.com>
2026-06-13 20:13:10 +0530

    first commit

---
+++ basics.sql
@@ -0,0 +1,9 @@
+create database collage;
+use collage;
+create table students(id int primary key,name varchar(50), age int not null);
+
+insert into students values(1,"harsh",23);
+
+insert into students values(2,"amrut",23);
+
+select * from students;
