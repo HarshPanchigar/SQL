0059b41bb2ba7f6afcf06dca893df8f67bfc60f2
HarshPanchigar <harshpanchigar@gmail.com>
2026-06-19 23:49:38 +0530

    initial commit

--- postgresql/task.sql
+++ postgresql/task.sql
@@ -52,3 +52,113 @@ VALUES
     ('Keyboard', 800.00),
     ('Cable', 250.00)
 ;
+
+
+select 
+    c.cust_name,
+    o.ord_date,
+    p.p_name,
+    p.price,
+    oi.quantity,
+    (oi.quantity * p.price) as total_price
+from order_items oi
+JOIN products p
+ON oi.p_id = p.p_id
+JOIN orders o
+ON o.ord_id = oi.ord_id
+JOIN customers c 
+ON o.cust_id = c.cust_id;
+
+CREATE VIEW billing_info as 
+select 
+    c.cust_name,
+    o.ord_date,
+    p.p_name,
+    p.price,
+    oi.quantity,
+    (oi.quantity * p.price) as total_price
+from order_items oi
+JOIN products p
+ON oi.p_id = p.p_id
+JOIN orders o
+ON o.ord_id = oi.ord_id
+JOIN customers c 
+ON o.cust_id = c.cust_id;
+
+select p_name,sum(total_price)
+from billing_info 
+GROUP BY p_name
+HAVING sum(total_price) > 1500;
+
+create or REPLACE PROCEDURE add_employee(
+    p_emp_id INT,
+    p_fname VARCHAR,
+    p_lname VARCHAR,
+    p_email VARCHAR,
+    p_dept VARCHAR,
+    p_salary NUMERIC
+)
+LANGUAGE plpgsql
+as $$
+BEGIN
+INSERT INTO employees(emp_id,fname,lname,email,dept,salary)
+VALUES (p_emp_id,p_fname,p_lname,p_email,p_dept,p_salary);
+end;
+$$;
+
+select e.fname,e.lname,e.salary 
+from employees e
+where 
+e.dept = 'HR' 
+and 
+e.salary = (
+    select max(emp.salary)
+    from employees emp
+    where emp.dept = 'HR');
+
+-- -----------------------------------------------------------
+-- FUNCTION
+
+CREATE OR REPLACE FUNCTION dept_max_sal_emp(
+    dept_name VARCHAR
+)
+RETURNS TABLE (
+    emp_id INT,
+    fname VARCHAR,
+    lname VARCHAR,
+    salary NUMERIC
+)
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        e.emp_id,
+        e.fname,
+        e.lname,
+        e.salary
+    FROM employees AS e
+    WHERE e.dept = dept_max_sal_emp.dept_name
+      AND e.salary = (
+            SELECT MAX(emp.salary)
+            FROM employees emp
+            WHERE emp.dept = dept_max_sal_emp.dept_name
+      );
+END;
+$$ LANGUAGE plpgsql;
+
+select * from dept_max_sal_emp('IT');
+
+select fname,salary,sum(salary) over(order by salary) from employees;
+select row_number() over(partition by dept),fname,dept,salary from employees;
+select fname ,salary , rank() over(order by salary desc) from employees;
+select fname ,salary , dense_rank() over(order by salary desc) from employees;
+select fname ,salary , lag(salary) over() from employees;
+select fname ,salary , lead(salary) over() from employees;
+
+with avg_sal as (select dept , avg(salary) as avg_salary from employees group by dept)
+select  e.emp_id,e.fname ,e.dept , e.salary , a.avg_salary::int from employees e join avg_sal a on e.dept = a.dept where  e.salary > a.avg_salary;
+
+with max_sal as (select dept , max(salary) as max_salary from employees group by dept)
+select e.emp_id,e.fname ,e.dept , e.salary from employees e 
+join max_sal m on e.dept = m.dept where  e.salary = m.max_salary;
+
